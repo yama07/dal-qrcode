5b865be7bf69d8becb7942bccb9612a60340c0e5
yama07 <12108794+yama07@users.noreply.github.com>
2025-07-30 23:58:51 +0900

    :bento: Add app icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  modules: ['@wxt-dev/module-svelte', '@wxt-dev/auto-icons'],
+  autoIcons: {
+    baseIconPath: 'assets/icon.svg',
+  },
+  webExt: {
+    startUrls: ['https://wxt.dev'],
+    openDevtools: true,
+  },
 });
