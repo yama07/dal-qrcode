b7f3e8c4af9486d56e44c56b8f0f7b1945f69f2e
yama07 <12108794+yama07@users.noreply.github.com>
2025-08-17 17:37:35 +0900

    :art: Improve QR code generator popup

--- src/entrypoints/popup/Generator.svelte
+++ src/entrypoints/popup/Generator.svelte
@@ -1,14 +1,52 @@
 <script lang="ts">
-  import Button from '$lib/components/ui/button/button.svelte';
-  import Textarea from '$lib/components/ui/textarea/textarea.svelte';
-  import QrCodeImage from '$lib/QrCodeImage.svelte';
+  import QRCode from 'qrcode';
+  import AppIcon from '~icons/assets/icon';
+  import MdiAlertCircleOutline from '~icons/mdi/alert-circle-outline';
+  import MdiContentCopy from '~icons/mdi/content-copy';
+  import MdiTrayDownload from '~icons/mdi/tray-download';
 
+  import { Alert, AlertDescription } from '$lib/components/ui/alert';
+  import Button from '$lib/components/ui/button/button.svelte';
+  import { Card, CardContent } from '$lib/components/ui/card';
+  import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+
+  type AppState =
+    | { state: 'idle' }
+    | { state: 'scanning' }
+    | { state: 'error'; error: Error }
+    | {
+        state: 'completed';
+        result: { imgDataUrl: string };
+      };
+
+  let appState = $state<AppState>({ state: 'idle' });
   let text: string = $state('');
-  let dataUrl: string = $state('');
 
   browser.tabs.query({ active: true, currentWindow: true }).then((tab) => {
     text = tab[0]?.url ?? '';
     console.debug('tab:', text);
+  });
+
+  $effect(() => {
+    appState = { state: 'scanning' };
+    QRCode.toDataURL(text, {
+      margin: 2,
+      color: {
+        dark: '#000000',
+        light: '#ffffff',
+      },
+      scale: 4,
+      width: 512,
+    })
+      .then((result) => {
+        appState = { state: 'completed', result: { imgDataUrl: result } };
+      })
+      .catch((error: Error) => {
+        appState =
+          error.message === 'No input text'
+            ? { state: 'idle' }
+            : { state: 'error', error };
+      });
   });
 
   function dataUrlToBlob(dataUrl: string) {
@@ -24,7 +62,7 @@
     return new Blob([byteArray], { type: mimeString });
   }
 
-  function onDownloadClick() {
+  function download(dataUrl: string) {
     const blob = dataUrlToBlob(dataUrl);
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
@@ -35,7 +73,7 @@
     URL.revokeObjectURL(link.href);
   }
 
-  async function onCopyClick() {
+  async function copyToClipboard(dataUrl: string) {
     const blob = dataUrlToBlob(dataUrl);
     const item = new ClipboardItem({ [blob.type]: blob });
     await navigator.clipboard.write([item]);
@@ -43,20 +81,59 @@
 </script>
 
 <div class="grid gap-4">
-  <QrCodeImage {text} size={256} bind:dataUrl />
+  <Card class="aspect-square p-0">
+    <CardContent class="p-0">
+      {#if appState.state === 'idle' || appState.state === 'scanning' || appState.state === 'error'}
+        <AppIcon
+          role="img"
+          aria-label="icon"
+          class="text-neutral-600 size-full p-8"
+        />
+      {:else if appState.state === 'completed'}
+        <img
+          src={appState.result.imgDataUrl}
+          alt="QR Code"
+          class="size-full p-1"
+        />
+      {/if}
+    </CardContent>
+  </Card>
 
   <Textarea
     bind:value={text}
-    placeholder="Enter text or URL to generate QR code"
+    placeholder={browser.i18n.getMessage('popup.generate.textarea-placeholder')}
     aria-label="QR Code Input"
   />
 
-  <div class="flex justify-end gap-4">
-    <Button disabled={dataUrl === ''} onclick={onDownloadClick}>
-      {browser.i18n.getMessage('popup.generate.download_button')}
-    </Button>
-    <Button disabled={dataUrl === ''} onclick={onCopyClick}>
-      {browser.i18n.getMessage('popup.generate.copy_button')}
-    </Button>
-  </div>
+  {#if appState.state === 'error'}
+    <Alert variant="destructive">
+      <MdiAlertCircleOutline />
+      <AlertDescription>{appState.error.message}</AlertDescription>
+    </Alert>
+  {:else}
+    <div class="flex justify-end gap-4">
+      <Button
+        disabled={appState.state !== 'completed'}
+        onclick={() => {
+          if (appState.state === 'completed') {
+            download(appState.result.imgDataUrl);
+          }
+        }}
+      >
+        <MdiTrayDownload />
+        {browser.i18n.getMessage('popup.generate.download_button')}
+      </Button>
+      <Button
+        disabled={appState.state !== 'completed'}
+        onclick={() => {
+          if (appState.state === 'completed') {
+            copyToClipboard(appState.result.imgDataUrl);
+          }
+        }}
+      >
+        <MdiContentCopy />
+        {browser.i18n.getMessage('popup.generate.copy_button')}
+      </Button>
+    </div>
+  {/if}
 </div>
