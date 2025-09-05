d14c8370f03497c504fe553dc98d30c9eda2d36c
yama07 <12108794+yama07@users.noreply.github.com>
2025-09-06 00:48:39 +0900

    :wrench: Fix unit test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, defineConfig } from 'vitest/config';
 import { WxtVitest } from 'wxt/testing';
 
 export default defineConfig({
   test: {
     mockReset: true,
     restoreMocks: true,
+    exclude: [...configDefaults.exclude, 'e2e/**'],
   },
   plugins: [WxtVitest()],
 });
